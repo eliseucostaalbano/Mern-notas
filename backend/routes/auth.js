97bdef72b41f291c3af3921e1d46fa3dc99a50a5
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-07-08 13:30:50 -0300

    criando rotas

---
+++ backend/routes/auth.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import User from '../models/User.js';
+
+const router = express.Router(); 
+
+// Rota para obter informações do usuário]
+// router.get("/eu", protect, async (req, res) => {
+//   res.status(200).json(req.user);
+// });
+
+// Rota para registro de usuário
+router.post('/registro', async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    try {
+        if (!nome || !email || !senha) {
+            return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+        }
+
+        const userExistente = await User.findOne({email})
+        if (userExistente) {
+            return res.status(400).json({ message: 'Usuário já existe.' });
+        }
+
+        const user = await User.create({nome, email, senha});
+        return res.status(201).json({id:user._id, nome : user.nome, email: user.email });
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro no servidor.' });
+    }
+})
+
+// Rota para login de usuário
+router.post('/login', async (req, res) => {
+    const { email, senha } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+        if (!user || !(await user.matchPassword(senha))) {
+            return res.status(401).json({ message: 'Credenciais inválidas.' });
+        }
+        return res.status(200).json({ id: user._id, nome: user.nome, email: user.email }); 
+    } catch (error) {
+        res.status(500).json({ message: 'Erro no servidor.' });
+    }
+});
+
+export default router;
