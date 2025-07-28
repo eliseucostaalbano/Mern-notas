cf9a99a54f295191030fcb1667e0927cc8d13e32
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-07-28 13:56:08 -0300

    criando conexao de deploy

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,25 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import notasRoutes from './routes/notas.js';
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 2621;
-
 const app = express();
 app.use(express.json());
 
 app.use('/api/user', authRoutes);
 app.use('/api/notas', notasRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Ola, mundo! Servidor rodando com Express e Nodemon.');
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 connectDB();
 
